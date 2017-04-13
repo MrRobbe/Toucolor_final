93f2e7d09be74f769e53c8b1446a11eab3cc1520
Lorenzo De Bie <lorenzodebie@ndbsoft.be>
2017-04-13 17:46:03 +0200

    test

---
+++ src/Toucolor/Toucolor.java
@@ -0,0 +1,14 @@
+package Toucolor;
+
+/**
+ * Created by loren on 02/04/2017.
+ */
+import processing.core.PApplet;
+
+public class Toucolor  extends PApplet {
+    public static void main(String args[]) {
+        PApplet.main(new String[]{"Toucolor.Toucolor"});
+
+
+    }
+}
