f9d133cf12e665447492a3cce9b009bac30689a7
Lorenzo De Bie <lorenzodebie@ndbsoft.be>
2017-04-13 18:11:05 +0200

    test

--- src/Toucolor/Toucolor.java
+++ src/Toucolor/Toucolor.java
@@ -6,9 +6,35 @@ package Toucolor;
 import processing.core.PApplet;
 
 public class Toucolor  extends PApplet {
+
+    /**
+     * PRIVATE VARIABLES
+     */
+    //width & height of world
+    private int worldWidth = 1280;
+    private int worldHeight = 720;
+
+
     public static void main(String args[]) {
         PApplet.main(new String[]{"Toucolor.Toucolor"});
+    }
 
+    /**
+     * define and initialize settings for the world
+     */
+    @Override
+    public void settings() {
+        size(worldWidth, worldHeight);
+    }
+
+    /**
+     * HERE HAPPENS THE RENDERING
+     */
+    @Override
+    public void draw() {
 
     }
+
+
+
 }
