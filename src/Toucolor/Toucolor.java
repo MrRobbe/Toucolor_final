28bd254ff4fc4b5ed0bc944174c75b70fa964f28
VinceNaessens <vince.naessens01@gmail.com>
2017-04-13 18:14:49 +0200

    TC-Player:
    Toucoulor.class toegevoegd
    Player.class aangepast

--- src/Toucolor/Toucolor.java
+++ src/Toucolor/Toucolor.java
@@ -11,4 +11,91 @@ public class Toucolor  extends PApplet {
 
 
     }
+
+
+    //Wereld variabelen
+    int sizeX = 1280;
+    int sizeY = 720;
+    static int blockSize = 80;
+
+    //Player variabelern
+    float xpos;
+    float ypos;
+    private Player speler = new Player();
+
+    //Alle enemies die bestaan met hun eigenschappen
+
+
+
+
+
+    @Override
+    public void settings() {
+        size(sizeX, sizeY);
+    }
+
+
+    @Override
+    public void setup(){
+        frameRate(144);
+    }
+
+    @Override
+    public void draw(){
+        background(225);
+        PlayerBehaviour();
+        drawCube();
+    }
+
+    public void tekenMap(float x, float y) {
+        fill(255,0,0);
+        rect(x,y,blockSize,blockSize);
+
+    }
+
+    public void drawCube() {
+        fill(255, 255, 0);
+        rect(speler.playerX, speler.playerY, blockSize, blockSize);
+    }
+
+    public void keyPressed() {
+
+        if (keyCode == RIGHT) {
+            speler.rightPressed = true;
+        }
+        if (keyCode == LEFT) {
+            speler.leftPressed = true;
+        }
+        if (keyCode == UP && !speler.isInAir) {
+            speler.upIsPressed = true;
+        }
+        if(keyCode == DOWN){
+            speler.downPressed = true;
+        }
+
+    }
+
+    public void keyReleased() {
+        if(keyCode == RIGHT){
+            speler.rightPressed = false;
+        }
+        if(keyCode == LEFT){
+            speler.leftPressed = false;
+        }
+        if(keyCode == DOWN){
+            speler.downPressed = false;
+        }
+    }
+
+
+    private void PlayerBehaviour(){
+        for (float[] hoevcoord:Player.fullCoords) {
+            xpos = hoevcoord[0];
+            ypos = hoevcoord[1];
+            tekenMap(xpos,ypos);
+        }
+        speler.keyUse();
+    }
+
 }
+
